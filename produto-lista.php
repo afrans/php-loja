c30d86ac4561ca37f26d2ccee5e915ecc206c10e
Anderson Frans <andersonfrans@hotmail.com>
2015-05-22 16:24:16 -0300

    Add a funcao altera produto

--- produto-lista.php
+++ produto-lista.php
@@ -18,6 +18,7 @@ foreach($produtos as $produto) :
         <td><?= $produto['nome'] ?></td>
         <td><?= $produto['preco'] ?></td>
         <td><?= substr ($produto['descricao'],0,40) ?></td>
+        <td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id']?>">alterar</a>
         <td><?= $produto['categoria_nome'] ?></td> 
         <td>
 
